0024926c8675232f8b3aa7d2cfae353e8d795c9f
dmp450@USASK.CA <dmp450@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-09-15 03:44:11 +0000

    Wrote partA1.c so that it creates threads which do nothing.
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@11 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a1/partA1.c
+++ a1/partA1.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
-int main(int argc, char* argv[]){
-	printf("%s %s %s\n", argv[1], argv[2], argv[3]);
-	return 0;
+#include "common.h"
+
+/* need CreateThread(), GetSystemTime(), Sleep() */
+
+int main(int argc, char* argv[argc+1]){
+
+    HANDLE* thread_array;
+    int num_threads, size, deadline;
+
+    parse_args(&num_threads, &deadline, &size, argc, argv);
+
+    /* create space for the threads */
+    thread_array = malloc(sizeof(HANDLE) * num_threads);
+    if (thread_array == NULL) return EXIT_FAILURE;
+
+    /* create threads and stash them in thread array */
+    for (size_t i = 0; i < threads; i++){
+        thread_array[i] = CreateThread(
+                NULL,                   /* security attributes */
+                0,                      /* default stack size */
+                thread_main,       /* thread function */
+                &size,                   /* ptr to thread function argument */
+                0,                      /* default creation flag */
+                &dwThreadIdArray[i]);   /* thread identifier */
+    }
+
+    free(thread_array);
+	return EXIT_SUCCESS;
 }
