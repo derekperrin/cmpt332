5af9a9fc11c5b3ee637a57f98c7d3b07e51eb590
dom258@USASK.CA <dom258@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-09-15 18:01:03 +0000

    Added skeleton files for partA2, partA3, and partA4
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@13 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a1/partA4.c
+++ a1/partA4.c
@@ -1,3 +1,13 @@
-int main(void) {
-	return 0;
+/* CMPT 332 -- Fall 2017
+* Assignment 1
+* Derek Perrin 		dmp450 11050915
+* Dominic McKeith 	dom258 11184543
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[argc+1]){
+	printf("Got to procedure main A4\n");
+	return EXIT_SUCCESS;
 }
