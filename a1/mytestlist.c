83ff9707e3c12dead68d13226395daaa1937bc57
dom258@USASK.CA <dom258@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-09-16 00:38:18 +0000

    Add function calls to mytestlist.c
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@17 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a1/mytestlist.c
+++ a1/mytestlist.c
@@ -1,9 +1,32 @@
+/* CMPT 332 -- Fall 2017
+* Assignment 1
+* Derek Perrin 		dmp450 11050915
+* Dominic McKeith 	dom258 11184543
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "list.h"
 
 int main(int argc, char* argv[]){
-    printf("This is a dummy test.\n");
+    printf("Got to procedure main\n");
+	
+	ListCreate();
+	ListCount(NULL);
+	ListFirst(NULL);
+	ListLast(NULL);
+	ListNext(NULL);
+	ListPrev(NULL);
+	ListCurr(NULL);
+	ListAdd(NULL, 0);
+	ListInsert(NULL, 0);
+	ListPrepend(NULL, 0);
+	ListRemove(NULL);
+	ListConcat(NULL, NULL);
+	ListFree(NULL, NULL);
+	ListTrim(NULL);
+	ListSearch(NULL, NULL, 0);
+	
     return EXIT_SUCCESS;
 }
