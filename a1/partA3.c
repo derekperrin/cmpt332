20b4245e42de515613799e200c6a04a61a258645
dmp450@USASK.CA <dmp450@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-09-25 22:59:56 +0000

    Begin part A3. Compiles and creates threads
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@44 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a1/partA3.c
+++ a1/partA3.c
@@ -6,8 +6,50 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+#include "common.h"
+
+void incr_func(void) {
+
+}
+
+void error_exit(char* error_message){
+    fprintf(stderr, "%s", error_message);
+    exit(EXIT_FAILURE);
+}
+
+void* thread_func(void* args) {
+    return 0;
+}
 
 int main(int argc, char* argv[argc+1]){
-    printf("Got to procedure main A3\n");
+    int error_code;
+    int num_threads, deadline, size;
+
+    if (parse_args(&num_threads, &deadline, &size, argc, argv) != EXIT_SUCCESS){
+        arg_error();
+    }
+
+    pthread_t* thread_array;
+
+    if ((thread_array = malloc(sizeof(pthread_t) * num_threads)) == NULL)
+        error_exit("malloc error partA3.c\n");
+
+    for (size_t i = 0; i < num_threads; ++i) {
+
+        error_code = pthread_create(
+                        thread_array + i,    /* array of threads */
+                        NULL,               /* default attributes */
+                        thread_func,        /* thread routine */
+                        &size);              /* args */
+        if (error_code != 0) {
+            printf("pthread_create error code %d\n", error_code);
+            error_exit("pthread_create error partA3.c\n");
+        }
+    }
+
+
+
     return EXIT_SUCCESS;
 }
