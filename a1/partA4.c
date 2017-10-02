d144bd04392bbda88126bd3ad732c0efd4dd9e42
dom258@USASK.CA <dom258@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-10-02 03:13:51 +0000

    Begin part A4
    * create child processes
    * create timer
    
    
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@51 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a1/partA4.c
+++ a1/partA4.c
@@ -6,8 +6,77 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "common.h"
+
+#define MAX_COUNT 200
+
+size_t square_counts = 0;
+
+void incr_func(void){
+    square_counts++;
+}
+
+int child_func(int size){
+    
+    for (size_t i = 0; i < size; ++i){
+        Square(i);
+    }
+    
+    printf("I am the child :'( WAAAA!!!\n");
+    
+    return EXIT_SUCCESS;
+}
+
+int timer(int deadline, int num_procs){
+    printf("I am the timer!!\n");
+    
+    if (sleep(deadline) != 0){
+        /* hopefully the parent has killed */
+        return EXIT_SUCCESS;
+    }
+    for (size_t i = 0; i < num_procs; ++i){
+        /* send signal to child process to exit */
+        
+    }
+    
+    return EXIT_SUCCESS;
+}
 
 int main(int argc, char* argv[argc+1]){
-    printf("Got to procedure main A4\n");
+    int num_procs, deadline, size;
+    
+    pid_t pid;
+    
+    if (parse_args(&num_procs, &deadline, &size, argc, argv) != EXIT_SUCCESS){
+        arg_error();
+        return EXIT_FAILURE;
+    }
+    
+    size_t i = 0;
+    do {
+        pid = fork();
+    } while (pid != 0 && ++i < num_procs);
+    /* TODO: Consider changing parent's priority */
+    if (pid == 0){
+        return child_func(size);
+    }
+    
+    /* create the timer process */
+    pid = fork();
+    if (pid == 0)
+        return timer(deadline, num_procs);
+    
+    /* wait for all the children processes to exit */
+    /* less than or equal because +1 for the timer */
+    for (size_t i = 0; i <= num_procs; ++i)
+        if (waitpid(-1, NULL, 0) == -1){
+            printf("Parent wait error\n");
+            return EXIT_FAILURE;
+    }
+    
     return EXIT_SUCCESS;
 }
