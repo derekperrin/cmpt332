85a89f17a6207e1878eb7d53bebc9879d61dbc99
dom258@USASK.CA <dom258@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-10-31 07:35:13 +0000

    s-chat creates all needed threads.
    Next step is to have the threads do something.
    
    
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@88 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a2/s-chat.c
+++ a2/s-chat.c
@@ -9,6 +9,7 @@
 #include <list.h>
 #include <rtthreads.h>
 #include <limits.h>
+#include <string.h>
 
 
 void inputMessage(void){
@@ -51,9 +52,9 @@ int mainp(int argc, char* argv[]){
     pc_name = argv[2];
     their_port = atoi(argv[3]);
 
-    printf("%d %s %d\n", my_port, pc_name, their_port);
+    printf("Arguments: %d %s %d\n", my_port, pc_name, their_port);
     /* use seperate scopes to get around c90 conventions 
-       to make things more organized. */
+       and to make things more organized. */
     {
         /* set up the server thread */
         /*int server_pid;*/
@@ -92,28 +93,22 @@ int mainp(int argc, char* argv[]){
                                RTTUSR /* make it a user level thread */);
     }
     {
-    /* set up the sendMessage thread */
+        /* set up the sendMessage thread */
         RttThreadId* sm_id;
         char* sm_thread_name;
         RttSchAttr* sm_priority;
         int* sm_args;
 
-
-
         sm_id = malloc(sizeof(RttThreadId));
-        sm_thread_name = malloc(sizeof(char)*7);
-        sm_thread_name[0] = 's'; 
-        sm_thread_name[1] = sm_thread_name[4] = 'e'; 
-        sm_thread_name[2] = sm_thread_name[6] = 'r'; 
-        sm_thread_name[4] = 'v';
-        sm_thread_name[7] = '\0';
+        sm_thread_name = malloc(sizeof(char)*12);
+        strncpy(sm_thread_name, "sendMessage\0", 12);
 
         sm_priority = malloc(sizeof(RttSchAttr));
 
         sm_priority->startingtime.seconds = 0; /* Start time of 0? */
         sm_priority->startingtime.microseconds = 0; /* Start time of 0? */
         sm_priority->deadline.seconds = LONG_MAX; /*Deadline of the max 
-                                        because we don't want the thread to exit */
+                                    because we don't want the thread to exit */
         sm_priority->deadline.microseconds = LONG_MAX; /* "" */
         sm_priority->priority = 20; /* choose a value halfway between the 
                                            recommended values (10 to 30) */
@@ -121,12 +116,101 @@ int mainp(int argc, char* argv[]){
         sm_args = malloc(sizeof(int));
         *sm_args = 10;
 
-        /*server_pid = */RttCreate(sm_id, sendMessage, 2<<20 /* one megabyte */, 
+        /*sm_pid = */RttCreate(sm_id, sendMessage, 2<<20 /* one megabyte */, 
                                sm_thread_name, 
                                sm_args /* have no args for now */,
                                *sm_priority, 
                                RTTUSR /* make it a user level thread */);
-    
+    }
+    {
+        /* set up the printMessage thread */
+        RttThreadId* pm_id;
+        char* pm_thread_name;
+        RttSchAttr* pm_priority;
+        int* pm_args;
+
+        pm_id = malloc(sizeof(RttThreadId));
+        pm_thread_name = malloc(sizeof(char)*12);
+        strncpy(pm_thread_name, "printMessage\0", 12);
+
+        pm_priority = malloc(sizeof(RttSchAttr));
+
+        pm_priority->startingtime.seconds = 0; /* Start time of 0? */
+        pm_priority->startingtime.microseconds = 0; /* Start time of 0? */
+        pm_priority->deadline.seconds = LONG_MAX; /*Deadline of the max 
+                                    because we don't want the thread to exit */
+        pm_priority->deadline.microseconds = LONG_MAX; /* "" */
+        pm_priority->priority = 20; /* choose a value halfway between the 
+                                           recommended values (10 to 30) */
+
+        pm_args = malloc(sizeof(int));
+        *pm_args = 10;
+
+        /*sm_pid = */RttCreate(pm_id, printMessage, 2<<20 /* one megabyte */, 
+                               pm_thread_name, 
+                               pm_args /* have no args for now */,
+                               *pm_priority, 
+                               RTTUSR /* make it a user level thread */);
+    }
+    {
+        /* set up the recieveMessage thread */
+        RttThreadId* rm_id;
+        char* rm_thread_name;
+        RttSchAttr* rm_priority;
+        int* rm_args;
+
+        rm_id = malloc(sizeof(RttThreadId));
+        rm_thread_name = malloc(sizeof(char)*15);
+        strncpy(rm_thread_name, "recieveMessage\0", 15);
+
+        rm_priority = malloc(sizeof(RttSchAttr));
+
+        rm_priority->startingtime.seconds = 0; /* Start time of 0? */
+        rm_priority->startingtime.microseconds = 0; /* Start time of 0? */
+        rm_priority->deadline.seconds = LONG_MAX; /*Deadline of the max 
+                                    because we don't want the thread to exit */
+        rm_priority->deadline.microseconds = LONG_MAX; /* "" */
+        rm_priority->priority = 20; /* choose a value halfway between the 
+                                           recommended values (10 to 30) */
+
+        rm_args = malloc(sizeof(int));
+        *rm_args = 10;
+
+        /*rm_pid = */RttCreate(rm_id, recieveMessage, 2<<20 /* one megabyte */,
+                               rm_thread_name, 
+                               rm_args /* have no args for now */,
+                               *rm_priority, 
+                               RTTUSR /* make it a user level thread */);
+    }
+    {
+        /* set up the inputMessage thread */
+        RttThreadId* im_id;
+        char* im_thread_name;
+        RttSchAttr* im_priority;
+        int* im_args;
+
+        im_id = malloc(sizeof(RttThreadId));
+        im_thread_name = malloc(sizeof(char)*15);
+        strncpy(im_thread_name, "recieveMessage\0", 15);
+
+        im_priority = malloc(sizeof(RttSchAttr));
+
+        im_priority->startingtime.seconds = 0; /* Start time of 0? */
+        im_priority->startingtime.microseconds = 0; /* Start time of 0? */
+        im_priority->deadline.seconds = LONG_MAX; /*Deadline of the max 
+                                    because we don't want the thread to exit */
+        im_priority->deadline.microseconds = LONG_MAX; /* "" */
+        im_priority->priority = 20; /* choose a value halfway between the 
+                                           recommended values (10 to 30) */
+
+        im_args = malloc(sizeof(int));
+        *im_args = 10;
+
+        /*im_pid = */RttCreate(im_id, inputMessage, 2<<20 /* one megabyte */, 
+                               im_thread_name, 
+                               im_args /* have no args for now */,
+                               *im_priority, 
+                               RTTUSR /* make it a user level thread */);
     }
     return 0;
 }
