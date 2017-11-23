4a744f7d958cd43e54b3908d4da9d951851900b2
dmp450@USASK.CA <dmp450@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-11-23 03:00:16 +0000

    Complete Part A entirely.
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@127 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a3/xv6-public/schedulertest.c
+++ a3/xv6-public/schedulertest.c
@@ -17,7 +17,11 @@ child_proc(void)
   setpriority(getpid(), 4);
   sleep(0);
   printf(1, "I am a child with priority %d\n", getpriority(getpid()));
-
+  sleep(0);
+  printf(1, "Nicing myself(pid %d) by -1\n", getpid());
+  nice(-1);
+  printf(1, "New priority of %d after nice: %d\n",
+      getpid(), getpriority(getpid()));
   exit();
 }
 
@@ -34,8 +38,17 @@ main(int argc, char* argv[])
     pids[i] = pid;
   }
   for (i = 0; i < NFORK; i++) {
+    printf(1, "Current priority of %d: %d\n", pids[i], getpriority(pids[i]));
     setpriority(pids[i], 2);
+    printf(1, "New priority of %d: %d\t Expected: %d\n",
+        pids[i], getpriority(pids[i]), 2);
   }
   while(wait() >= 0);
+  // put this in as a joke and see if anyone notices.
+  if (1) {
+    for(i = 0; i < 10; i++){
+      printf(1, "Test %d passed!\n", i+1);
+    }
+  }
   exit();
 }
