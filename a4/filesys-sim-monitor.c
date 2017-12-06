1788ba6ddf305ad88c72afcd24dfab30d0da27c4
dom258@USASK.CA <dom258@USASK.CA@f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5>
2017-12-06 01:27:45 +0000

    Started setting up the file system. Now it looks like Calculus is needed. There must be a better way to do this....
    git-svn-id: https://svn.cs.usask.ca/svn/student/332/group23@134 f1dc4cae-a7f4-4e0f-997a-bd0ec5f3fbb5

--- a4/filesys-sim-monitor.c
+++ a4/filesys-sim-monitor.c
@@ -15,19 +15,116 @@
 #include <os.h>
 #include <standards.h>
 
+char* disk; /* char because sizeof(char) = 1
+               (This makes pointer arithmetic easy) */
+char* superblock;
+char* remaining_blocks;
 
-void StartRead(void){
+int num_inodes;
 
+struct superblock {
+    struct free_block* fsl; /* Free Space List */
+    struct inode* root_dir;
+    struct inode* free_inodes;
+};
+
+struct inode {
+    int type;
+    char* data_block_0;
+    char* data_block_1;
+    char* data_block_2;
+    char* data_block_3;
+    struct inode* next_free_inode; /* Used to keep track of the free inodes,
+                                      just like how we did with our list library
+                                      for free lists. A better way to do this in
+                                      terms of memory managment would be to use
+                                      a union instead of another field in this
+                                      struct. */
+};
+
+struct free_block {
+    char* this_block;
+    char* next_block;
+};
+
+int my_mkdir(void){
+    return EXIT_SUCCESS;
 }
 
-void StopRead(void){
-
+int my_cd(void){
+    return EXIT_SUCCESS;
 }
 
-void StartWrite(void){
-
+int my_open(void){
+    return EXIT_SUCCESS;
 }
 
-void StopWrite(void){
+int my_close(void){
+    return EXIT_SUCCESS;
+}
 
+int my_read(void){
+    return EXIT_SUCCESS;
+}
+
+int my_write(void){
+    return EXIT_SUCCESS;
+}
+
+int my_rm(void){
+    return EXIT_SUCCESS;
+}
+
+int my_rmdir(void){
+    return EXIT_SUCCESS;
+}
+
+int my_seek(void){
+    return EXIT_SUCCESS;
+}
+
+int my_fs(void){
+    int superblock_size;
+    int inode_total_size;
+    int fsl_total_size;
+    char* inode_memory;
+    int i;
+    struct inode* current_inode;
+    struct inode* next_inode;
+    /* ------------------------- Create the "disk" ------------------------- */
+    disk = malloc(BLOCK_SIZE * NUM_BLOCKS);
+
+    /* ----------------------- Set up the superblock ----------------------- */
+    superblock_size = 0;
+    do {
+        superblock_size += BLOCK_SIZE;
+    } while (superblock_size < sizeof(struct superblock));
+    superblock = disk; /* "Allocating" the super block as the first block */
+    remaining_blocks = disk + superblock_size;
+
+    /* ------------------- Set up the inode memory space ------------------- */
+    /* Make the amount of space inodes take up to be ~1% of the disk size */
+    /* (This is what Wikipedia says is a common heuristic for inode size) */
+    i = BLOCK_SIZE * NUM_BLOCKS / 100; /* inode size to be 1% of disk size */
+    /* Next, round up the inode size so it falls on a factor of BLOCK_SIZE */
+    inode_total_size = 0;
+    do {
+        inode_total_size += BLOCK_SIZE;
+    } while (inode_total_size < i);
+
+    num_inodes = inode_total_size / sizeof(struct inode);
+
+    inode_memory = remaining_blocks;
+    remaining_blocks = remaining_blocks + inode_total_size;
+
+    /* ------------ Set up free inode list and put in superblock ------------ */
+    superblock->free_inodes = inode_memory;
+    for(i = 0; i < num_inodes - 1; i += 1){
+        current_inode = inode_memory + i * sizeof(struct inode);
+        next_inode = current_inode + 1;
+        current_inode->next_free_inode = next_inode;
+    }
+
+    /* ------------ -Calculate the number of nodes for the FSL ------------- */
+    /* And now I am stuck........... */
 }
--- a4/filesys-sim-monitor.h
+++ a4/filesys-sim-monitor.h
@@ -7,9 +7,18 @@
 #ifndef _FILESYS_SIM_MONITOR_H
 #define _FILESYS_SIM_MONITOR_H
 
-void StartRead(void);
-void StopRead(void);
-void StartWrite(void);
-void StopWrite(void);
+#define BLOCK_SIZE 4096
+#define NUM_BLOCKS 64;
+
+int my_mkdir(void);
+int my_cd(void);
+int my_open(void);
+int my_close(void);
+int my_read(void);
+int my_write(void);
+int my_rm(void);
+int my_rmdir(void);
+int my_seek(void);
+
 
 #endif /* _READER_WRITER_MONITOR_H */
